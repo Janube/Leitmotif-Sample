652dd6a48bbf721fcc3306eb7b8bcef51f40e5c0
Janube <wade.a.mcgrath@gmail.com>
2024-10-25 04:35:51 -0400

    v0.5 - Full functionality - No Worm
    Title

---
+++ yellow_sign.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+@onready var yellow_interact = false
+@onready var ui_scene = preload("res://ui_label.tscn")
+@onready var ui_label = ui_scene.instantiate()
+
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("player"):
+		ui_label = ui_scene.instantiate()
+		yellow_interact = true
+		get_parent().add_child(ui_label)
+		ui_label.global_position = $Sprite2D.global_position - Vector2(30,50)
+		ui_label.modulate.a = 0
+		var ui_tween = get_tree().create_tween()
+		ui_tween.tween_property(ui_label,"modulate:a", 1, .5)
+
+func _on_hitbox_body_exited(body):
+	if body.is_in_group("player"):
+		yellow_interact = false
+		if is_instance_valid(ui_label):
+			ui_label.despawn()
+		
+func talking():
+	ui_label.despawn()
