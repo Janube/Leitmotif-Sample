e9ce5a113aafac073df09ecefce060e4fd96e669
Janube <wade.a.mcgrath@gmail.com>
2024-10-26 20:41:34 -0400

    v0.73
    Updates to dialogue.
    Fixed physics when interacting with sign

--- cat!.gd
+++ cat!.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 @onready var root = get_node("/root/Game Level")
+@onready var input = true
 
 
 
@@ -15,15 +16,17 @@ func _ready():
 	add_to_group("player")
 	
 func _physics_process(delta):
-
 	var input_direction = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
 	update_animation_parameters(input_direction)
-	velocity.x = input_direction * move_speed
+	if input == true:
+		velocity.x = input_direction * move_speed
+	else:
+		velocity.x = 0
 	move_and_slide()
 	
 	if not is_on_floor():
 		velocity.y = velocity.y + (fall_acceleration * delta)
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
+	if is_on_floor() and Input.is_action_just_pressed("jump") and input == true:
 		velocity.y -= jump_impulse
 		
 func update_animation_parameters(input_direction : float):
@@ -38,11 +41,12 @@ func pick_new_state():
 		state_machine.travel("idle")
 
 func dialogue():
-	set_physics_process(false)
+	input = false
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
 	
 
 func _on_timeline_ended():
+	input = true
 	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
 	set_physics_process(true)
 	root.done_talking()
